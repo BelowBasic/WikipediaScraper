e3104843a99c337783f1221b609cf5e6cb82d1cc
clarameyer <clara145@hotmail.de>
2024-05-31 15:19:18 +0200

    changed window

--- addon/addon.py
+++ addon/addon.py
@@ -3,18 +3,61 @@ import xbmcgui
 import xbmc
 import wikipedia
 import logging
-import lxml
+# import lxml
 from bs4 import BeautifulSoup
 
+logging.basicConfig(level=logging.DEBUG)
 
-#region Functions
+
+class CustomWindow(xbmcgui.Window):
+    def __init__(self):
+        super().__init__()
+
+        if lang == "de-de":
+            content = scraper_de(query, "Film")
+        else:
+            content = scraper(query, "Film")
+        if not query:
+            data = "No Input"
+        else:
+            if content is not None:
+                data = str(content)
+                logging.debug(data)
+
+
+
+        # logging.debug(f"Type self.blub: {type (self.blub)} ____type of blub{type (blub)}")
+
+        # Set background color or image
+        self.background = xbmcgui.ControlImage(0, 0, 1280, 720, 'special://home/addons/skin.estuary/background.jpg')
+        self.addControl(self.background)
+
+        # Add a label to show the retrieved data
+        self.label = xbmcgui.ControlLabel(100, 100, 1080, 600, data, textColor='0xFFFFFFFF', font='font14')
+        self.addControl(self.label)
+
+        # Add a button to close the window
+        self.button = xbmcgui.ControlButton(100, 200, 200, 50, 'Close', textColor='0xFFFFFFFF', font='font14')
+        self.addControl(self.button)
+
+        # Add an image
+        self.image = xbmcgui.ControlImage(300, 200, 200, 200,
+                                          'special://home/addons/skin.estuary/media/default_icon.png')
+        self.addControl(self.image)
+
+    def onAction(self, action):
+        if action.getId() in [xbmcgui.ACTION_PREVIOUS_MENU, xbmcgui.ACTION_NAV_BACK]:
+            self.close()
+
+
+# region Functions
 def get_user_input():
     """Get User Input via Keyboard/Onscreen Keyboard"""
     kb = xbmc.Keyboard('', 'Please enter the video title')
     kb.doModal() # Onscreen keyboard appears
     if not kb.isConfirmed():
         return ""
-    query = kb.getText() # User input
+    query = kb.getText()  # User input
     return query
 
 
@@ -85,9 +128,9 @@ def scraper_de(movie_name, media_type):
     # Get Wikipedia page HTML
     search_result = wikipedia.search(movie_name + " " + media_type, suggestion=True)
     if len(search_result)<0:
-        return "Movie Not Found" 
+        return "Movie Not Found"
     if len(search_result[0])<0:
-        return "Movie Not Found" 
+        return "Movie Not Found"
     logging.debug(search_result)
     html = wikipedia.page(title=search_result[0][0], auto_suggest=False, redirect=False).html()
     # Initializes BeautifulSoup object of the HTML
@@ -108,27 +151,23 @@ def scraper_de(movie_name, media_type):
                 value = td.get_text(";", strip=True).replace("\xa0", " ")
                 infos.update({key: value.split(";")})
     return infos
-#endregion
-#region Main
+# end region
+
+# region Main
+
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
-
 
 # Set a string variable to use
 # line1 = "Hello World! We can write anything we want here Using Python"
 # Launch a dialog box in kodi showing the string variable 'line1' as the contents
-query = get_user_input() # User input via onscreen keyboard
+query = get_user_input()  # User input via onscreen keyboard
 lang = xbmc.getLanguage(xbmc.ISO_639_1)
-if lang == "de-de":
-   content = scraper_de(query,"Film")
-else:
-    content = scraper(query,"Film")
-if not query:
-    xbmcgui.Dialog().ok(addonname, "No Input")
-else: 
-    if content is not None:
 
-        formatted_content = format_dict(content)
-        formatted_content = formatted_content.replace("[", "").replace("]", "").replace("'", "")
-        xbmcgui.Dialog().ok(addonname, formatted_content)
-#endregion
+window = CustomWindow()
+window.show()
+
+while not xbmc.Monitor().waitForAbort(1):
+    xbmc.sleep(100)
+
+# end region
