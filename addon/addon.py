daa0aeb7a881cb11732f77d860b010fabfb47541
luigigalbusera <galbu.luigi@gmail.com>
2024-06-02 17:39:52 +0200

    Change comment the code

--- addon/addon.py
+++ addon/addon.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 logging.basicConfig(level=logging.DEBUG)
 global_image_url = None
 
+# The method will receive a dictionary and return a string that is better suited for parsing in Kodi.
 def format_dict(dictionary):
     formatted_output = ""
     stack = [(dictionary, 0)]  # (dict, indentation level)
@@ -21,6 +22,7 @@ def format_dict(dictionary):
             else:
                 formatted_output += "  " * indent + f"{key}: {value}\n"
     return formatted_output
+
 
 
 class CustomWindow(xbmcgui.Window):
@@ -78,101 +80,97 @@ def get_user_input():
 
 
 def scraper(movie_name, media_type):
-    """Gets Wikipedia Page and parses Infobox"""
+
+    """Gets the Wikipedia page and parses the infobox.
+
+    Args:
+        movie_name (str): The name of the movie to search for on Wikipedia.
+        media_type (str): The type of media, such as "Film" or "TV series".
+
+    Returns:
+        dict: A dictionary containing information parsed from the Wikipedia infobox.
+    """
+
     global global_image_url
-    # Get Wikipedia page HTML
     search_result = wikipedia.search(movie_name + " " + media_type, suggestion=True)
     logging.debug(search_result)
     html = wikipedia.page(title=search_result[0][0], auto_suggest=False, redirect=False).html()
-    # Initializes BeautifulSoup object of the HTML
     html_soup = BeautifulSoup(html, features="html.parser")
-    # Search for first instance of tables of class infobox and find all instances of "tr"
     infobox_html = html_soup.find('table', class_='infobox').find_all('tr')
-    # Initializes an empty dictionary for the output data
     infos = {}
-    # iterate through the infobox "tr"s
     for x in infobox_html:
-        # find first instance of a "th" tag
         th = x.find('th')
-        # checks if an instance of "th" is found
-        if th is not None:
+        if th:
             if th["class"] == ["infobox-above", "summary"]:
                 infos.update({"Title": th.get_text(strip=True).replace("\xa0", " ")})
-            # create variable key with the text inside the th tag
             key = th.get_text(" ", strip=True)
-            # find all instances of "li" tags
             li = x.find_all('li')
-            # Initialize the list value
             value = []
-            # If an instance of "li" exists iterate through them and add the text inside to the value list
-            if li is not None and li != []:
+            if li:
                 for y in li:
                     value.append(y.get_text(" ", strip=True).replace("\xa0", " "))
-            # If no instance of "li" occurs search for instances of "td" and iterate through them
-            # and add the text inside to the value list
             else:
                 td = x.find_all('td')
-                if td is not None:
+                if td:
                     for z in td:
                         value.append(z.get_text(" ", strip=True).replace("\xa0", " "))
             if value:
                 infos.update({key: value})
         else:
             td = x.find('td', class_="infobox-image")
-            if td is not None:
+            if td:
                 logging.debug({"Cover image": td.find("img")["src"].strip("//")})
                 global_image_url = 'https://' + td.find("img")["src"].strip("//")
                 infos.update({"Cover image": global_image_url})
-
     return infos
 
 
 def scraper_de(movie_name, media_type):
-    """Gets German Wikipedia Page and parses Infobox"""
-    wikipedia.set_lang("de")
-    # Get Wikipedia page HTML
+    """Gets German Wikipedia Page and parses Infobox.
+    
+    The return value of wikipedia.search is a tuple containing two elements:
+
+    A list of page titles matching the search query.
+    A suggested title for the search query.
+
+    """
+    
+    wikipedia.set_lang("de") 
     search_result = wikipedia.search(movie_name + " " + media_type, suggestion=True)
-    if len(search_result)<0:
+    if not search_result:
         return "Movie Not Found"
-    if len(search_result[0])<0:
-        return "Movie Not Found"
-    logging.debug(search_result)
+    
     html = wikipedia.page(title=search_result[0][0], auto_suggest=False, redirect=False).html()
-    # Initializes BeautifulSoup object of the HTML
     html_soup = BeautifulSoup(html, features="html.parser")
-    # Search for first instance of tables of class infobox and find all instances of "tr"
     infobox_html = html_soup.find('table', class_='infobox').find_all('tr')
-    # Initializes an empty dictionary for the output data
     infos = {}
-    # iterate through the infobox "tr"s
-    for x in infobox_html:
-        # find first instance of a "th" tag
-        th = x.find('th')
-        # checks if an instance of "th" is found
-        if th is not None:
-            key = th.get_text(strip=True).replace("\xa0", " ")
-            td = x.find('td')
-            if td is not None:
-                value = td.get_text(";", strip=True).replace("\xa0", " ")
-                infos.update({key: value.split(";")})
+    for row in infobox_html:
+        header = row.find('th')
+        if header is not None:
+            key = header.get_text(strip=True).replace("\xa0", " ")
+            data = row.find('td')
+            if data is not None:
+                value = data.get_text(";", strip=True).replace("\xa0", " ")
+                infos.update({key: value.split(";")})   
     return infos
+
 # end region
 
-# region Main
 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
+# Initialize Kodi add-on information
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
 
-# Set a string variable to use
-# line1 = "Hello World! We can write anything we want here Using Python"
-# Launch a dialog box in kodi showing the string variable 'line1' as the contents
+# Prompt user for input
 query = get_user_input()  # User input via onscreen keyboard
 lang = xbmc.getLanguage(xbmc.ISO_639_1)
 
+# Create and display custom window
 window = CustomWindow()
 window.show()
 
+# Main loop to keep the add-on running
 while not xbmc.Monitor().waitForAbort(1):
     xbmc.sleep(100)
 
-# end region
+
